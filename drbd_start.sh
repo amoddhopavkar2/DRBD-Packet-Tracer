3743c1e58156943bd9e18e9649133319d0bc06c7
bharatKothari <bharatkothari1008@gmail.com>
2021-03-17 10:55:35 -0400

    minor

--- drbd_start.sh
+++ drbd_start.sh
@@ -1,11 +1,13 @@
 dir=$(pwd)
 echo dir
 cd $dir/tracer
+make clean
 make
 gcc tracer.c -o tracer
 cd $dir
 cp $dir/tracer/Modules.symvers ./drbd-8.4/drbd
 cd $dir/drbd-8.4
+make clean
 make
 insmod $dir/tracer/tracer.ko
 insmod $dir/drbd-8.4/drbd/drbd.ko
